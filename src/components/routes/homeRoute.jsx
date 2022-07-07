5de45b3bf6b6c8b77dd0135188055275467d479f
Temitope Samson Isaiah <61621188+smit1298@users.noreply.github.com>
2022-07-07 08:21:57 +0100

    Image responsiveness
    responsiveness for all pages

--- src/components/routes/homeRoute.jsx
+++ src/components/routes/homeRoute.jsx
@@ -16,9 +16,8 @@ function Home() {
   return (
     <div className="home-route">
       <Hero />
-      {/* <Image /> */}
-      
-      {/* <Brands /> */}
+      <Image />
+            {/* <Brands /> */}
        {/* <Features /> */}
       {/*<Chateau />
       <Benefits />
